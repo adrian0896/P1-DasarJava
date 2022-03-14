bb8045b2dc0e61309d9f4cc9dd8b20ff86bf5ab4
DELL <marshelcristiany201>
2022-03-14 17:34:26 +0700

    mencetak literal string

---
+++ src/M_LiteralString.java
@@ -0,0 +1,8 @@
+public class M_LiteralString
+{
+    public static void main(String[] args)
+    {
+    String welcome = "pemograman Beorentasi Object Dengan Java";
+    System.out.println(welcome);
+    }
+}
