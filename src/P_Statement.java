eed8b2834c9dcacc9dcd2e3e6e9117cab04441f1
DELL <marshelcristiany201>
2022-03-14 19:28:44 +0700

    contoh statement

---
+++ src/P_Statement.java
@@ -0,0 +1,12 @@
+public class P_Statement
+{
+    public static void main(String[] args)
+    {
+        System.out.println("semoga saya bisa belajar");
+        System.out.println("pemograman berorentasi objek dengan java");
+        int angka = 10;
+        char charA = 'A';
+        System.out.println("angka");
+        System.out.println("charA");
+    }
+}
