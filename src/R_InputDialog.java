435a5727b2cd292dd0b2e51b89bfaf449dce8a16
DELL <marshelcristiany201>
2022-03-14 19:59:50 +0700

    membaca masukkan dari keyboard dengan dialog box

---
+++ src/R_InputDialog.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class R_InputDialog
+{
+    public static void main(String[] args)
+    {
+    String name = JOptionPane.showInputDialog("what is your name");
+        System.out.println("my name is " + name);
+        System.exit(0);
+    }
+}
