a63fb7dd8c3c91b14efffbe1123a5073dc794054
DELL <marshelcristiany201>
2022-03-14 15:14:39 +0700

    inisial project pertemuan 1 dasar java

---
+++ src/A_BlokDanKomentar.java
@@ -0,0 +1,13 @@
+public class A_BlokDanKomentar {
+    public static void main(String[] args) {
+        System.out.println("Pemograman Borentasi Objek Dengan Java");
+        //contoh komentar satu baris
+
+        /*contoh komentar lebih satu baris
+
+         */
+        /**
+         * java Doc --> baris baris ini untuk mendokumentasikan kode program
+         */
+    }
+}
